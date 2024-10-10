5f670ba6dcc85059a6ff7be89373d0ae1bad2114
Demid <demid-aykashev@mail.ru>
2024-10-10 17:07:08 +0500

    fix: change aside logic

--- src/pages/Main/ui/Main.tsx
+++ src/pages/Main/ui/Main.tsx
@@ -1,37 +1,21 @@
 import { Header } from '@/widgets/Header';
 import { Aside } from '@/widgets/Aside';
-import { useCallback, useState } from 'react';
-import { ModalWrapper } from '@/shared/ui';
-import { CreateServiceForm } from '@/features/CreateService';
+import { useState } from 'react';
 import { ServicesPanel } from '@/widgets/ServicesPanel';
 
 export default function Main() {
-  const [isCreateModalOpen, setIsCreateModalOpen] = useState<boolean>(false);
   const [searchValue, setSearchValue] = useState<string>('');
 
-  const createHandler = useCallback(() => {
-    setIsCreateModalOpen((prevState) => !prevState);
-  }, []);
-
   return (
-    <>
-      <main>
-        <Header
-          searchValue={searchValue}
-          setSearchValue={setSearchValue}
-        />
-        <div className='grid grid-cols-[100px_1fr] grid-rows-1'>
-          <Aside createHandler={createHandler} />
-          <ServicesPanel searchValue={searchValue} />
-        </div>
-      </main>
-
-      {/* Create service modal */}
-      {isCreateModalOpen && (
-        <ModalWrapper closeHandler={createHandler}>
-          <CreateServiceForm closeModalHandler={createHandler} />
-        </ModalWrapper>
-      )}
-    </>
+    <main>
+      <Header
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
+      <div className='grid grid-cols-[100px_1fr] grid-rows-1'>
+        <Aside />
+        <ServicesPanel searchValue={searchValue} />
+      </div>
+    </main>
   );
 }
--- src/widgets/Aside/ui/Aside.tsx
+++ src/widgets/Aside/ui/Aside.tsx
@@ -1,26 +1,39 @@
 import { CirclePlus } from 'lucide-react';
-import { memo } from 'react';
+import { useCallback, useState } from 'react';
+import { ModalWrapper } from '@/shared/ui';
+import { CreateServiceForm } from '@/features/CreateService';
 
-interface AsideProps {
-  createHandler: () => void;
-}
+const Aside = () => {
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState<boolean>(false);
 
-const Aside = ({ createHandler }: AsideProps) => {
+  const createHandler = useCallback(() => {
+    setIsCreateModalOpen((prevState) => !prevState);
+  }, []);
+
   return (
-    <aside className='bg-accentGreen h-screen-minus-105 rounded-br-xl flex flex-col items-center justify-center'>
-      <div className='flex flex-col gap-10'>
-        <button
-          onClick={createHandler}
-          type='button'
-        >
-          <CirclePlus
-            size={60}
-            className='text-white hover:text-accentYellow duration-200 ease-in-out transition-colors'
-          />
-        </button>
-      </div>
-    </aside>
+    <>
+      <aside className='bg-accentGreen h-screen-minus-105 rounded-br-xl flex flex-col items-center justify-center'>
+        <div className='flex flex-col gap-10'>
+          <button
+            onClick={createHandler}
+            type='button'
+          >
+            <CirclePlus
+              size={60}
+              className='text-white hover:text-accentYellow duration-200 ease-in-out transition-colors'
+            />
+          </button>
+        </div>
+      </aside>
+
+      {/* Create service modal */}
+      {isCreateModalOpen && (
+        <ModalWrapper closeHandler={createHandler}>
+          <CreateServiceForm closeModalHandler={createHandler} />
+        </ModalWrapper>
+      )}
+    </>
   );
 };
 
-export default memo(Aside);
+export default Aside;
